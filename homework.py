8e0f51bf1a8b4e1a893d7b1fbaec4f98822dd6ec
Кагадий Андрей <kagadi.a@gmail.com>
2022-12-12 15:41:00 +0300

    modified:   homework.py

--- homework.py
+++ homework.py
@@ -3,19 +3,14 @@ import os
 import time
 
 import requests
+from dotenv import load_dotenv
 from telegram import Bot, ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Updater
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
 logging.basicConfig(
-    handlers=[
-        logging.FileHandler(
-            filename='main.log', encoding='utf-8'
-        )
-    ],
+    handlers=[logging.FileHandler(filename='main.log', encoding='utf-8')],
     format='%(asctime)s,  %(name)s, %(levelname)s, %(message)s',
     datefmt="%F %A %T",
     level=logging.INFO,
@@ -36,61 +31,76 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.',
 }
 
-status = ''
-
 
 def check_tokens():
+    """Проверка предзаполненых переменных окружения."""
     if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
-        logging.error(f'Отсутствует хотя бы одна переменная окружения')
+        logging.critical(f'Отсутствует хотя бы одна переменная окружения')
         raise ValueError(f'Отсутствует хотя бы одна переменная окружения!')
 
 
 def send_message(bot, message):
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    """Отправка собщения пользователю бота."""
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.debug(f'Сообщение отправлено: {message}')
+    except Exception as error:
+        logging.error(f'Упс сообщение не получилось отправить: {error}')
 
 
 def get_api_answer(timestamp):
+    """Запрос к эндпоинту API-сервиса."""
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=timestamp)
     except Exception as error:
         logging.error(f'Ошибка при запросе к основному API: {error}')
-
-    homework = response.json().get('homeworks')[0]
-    return parse_status(homework)
+    return response.json()
 
 
 def check_response(response):
-    ...
+    """Проверяет ответ API на соответствие документации."""
+    try:
+        homework = response[0]
+        return homework
+    except Exception as error:
+        logging.error(f'Ошибка при проверке запроса от API: {error}')
 
 
 def parse_status(homework):
-    global status
-    if homework['status'] != status:
-        status = homework['status']
+    """Извлекает статус работы."""
+    # if homework['status'] in HOMEWORK_VERDICTS:
+    try:
         homework_name = homework['homework_name']
-        verdict = HOMEWORK_VERDICTS[status]
+        verdict = HOMEWORK_VERDICTS[homework['status']]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    except Exception as error:
+        logging.error(f'Статус не в запросе не соответствует словарю - {error}')
 
 
 def main():
     """Основная логика работы бота."""
     check_tokens()
-
     bot = Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-    message = get_api_answer({'from_date': 0})
-    send_message(bot, message)
-    # ...
+    status = ''
+    while True:
+        try:
+            response = get_api_answer({'from_date': timestamp})
+            timestamp = response['current_date']
+            homework = check_response(response['homeworks'])
+            if homework and status != homework['status']:
+                status = homework['status']
+                message = parse_status(homework)
+                send_message(bot, message)
+                logging.debug(f'Сообщение отправленно со статусом - "{status}"')
+            print(f'Уже должно быть заполнено - {status}')
+            time.sleep(10)
 
-    # while True:
-    #     try:
-
-    #         ...
-
-    #     except Exception as error:
-    #         message = f'Сбой в работе программы: {error}'
-    #         ...
-    #     ...
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            send_message(bot, message)
+            raise ValueError(f'хз что но что-то не так!')
+        ...
 
 
 if __name__ == '__main__':
